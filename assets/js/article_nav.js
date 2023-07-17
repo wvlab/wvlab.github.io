8352de39994e4009bba630fae7002ee07fa158e4
WVlab <tabs3536@gmail.com>
2023-07-17 18:09:17 +0300

    cstructs progress

--- assets/js/article_nav.js
+++ assets/js/article_nav.js
@@ -1,24 +1,21 @@
-const body = document.body;
-
 window.addEventListener("DOMContentLoaded", () => {
-
-	const observer = new IntersectionObserver(entries => {
-        if (parseInt(body.style.maxWidth.substring(0, -3)) < 800) {
+    const observer = new IntersectionObserver(entries => {
+        if (window.innerWidth < 800) {
             return;
         }
 
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute("id");
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add("active");
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove("active");
-			}
-		});
-	});
+    	entries.forEach(entry => {
+    		const id = entry.target.getAttribute("id");
+    		if (entry.intersectionRatio > 0) {
+    			document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add("active");
+    		} else {
+    			document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove("active");
+    		}
+    	});
+    });
 
-	document.querySelectorAll("section[id]").forEach((section) => {
-		observer.observe(section);
-	});
-	
+    document.querySelectorAll("section[id]").forEach((section) => {
+    	observer.observe(section);
+    });
+    
 });
